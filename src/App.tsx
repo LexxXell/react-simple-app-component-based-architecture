6e6db118660925fa7353b9c17121b6066128a108
LexxXell <lexxxell007@gmail.com>
2023-11-27 16:27:31 +0400

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import Button from './components/ui/Button/Button';
 import EyeIcon from './components/ui/EyeIcon/EyeIcon';
 
 const App = () => {
   return (
     <>
       <EyeIcon width="18px" height="18px" isActive={true} onClick={() => console.log('EyeIcon clicked')} />
+      <br />
+      <Button
+        width="100px"
+        onClick={() => {
+          console.log('Button clicked');
+        }}
+      >
+        Ky
+      </Button>
     </>
   );
 };
