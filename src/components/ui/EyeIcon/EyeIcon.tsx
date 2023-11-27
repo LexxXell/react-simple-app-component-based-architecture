751f6dcd8456328e68cc860b68ef6d76dd1c115d
LexxXell <lexxxell007@gmail.com>
2023-11-27 16:26:36 +0400

    Review EyeIcon

--- src/components/ui/EyeIcon/EyeIcon.tsx
+++ src/components/ui/EyeIcon/EyeIcon.tsx
@@ -5,12 +5,12 @@ interface IEyeIconProps extends SVGProps<SVGSVGElement> {
   isActive?: boolean;
 }
 
-const EyeIcon: FC<IEyeIconProps> = ({ isActive: initActive = false, className, ...props }) => {
+const EyeIcon: FC<IEyeIconProps> = ({ onClick, isActive: initActive = false, className, ...props }) => {
   const [isActive, setIsActive] = useState(initActive);
 
   const onClickHandle = (e: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
     setIsActive(!isActive);
-    props.onClick?.(e);
+    onClick?.(e);
   };
 
   const combinedClassName = `${className || ''} ${isActive ? 'active' : 'idle'}`.trim();
