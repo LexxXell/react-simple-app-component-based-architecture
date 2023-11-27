071bd72738ba935fac5f94bdd88b78de2dd6fed6
LexxXell <lexxxell007@gmail.com>
2023-11-27 19:44:22 +0400

    Updated ui/Button

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,15 +1,24 @@
 import { FC, MouseEventHandler } from 'react';
 import ButtonView, { IButtonViewProps } from './ButtonView';
 
-interface IButtonProps extends IButtonViewProps {}
+import styles from './styles/ButtonView.module.scss';
+import classnames from 'classnames';
 
-const Button: FC<IButtonProps> = ({ children, onClick, ...props }) => {
+interface IButtonProps extends IButtonViewProps {
+  typeView?: 'def' | 'alt';
+}
+
+const Button: FC<IButtonProps> = ({ children, typeView = 'def', onClick, ...props }) => {
   const handleClick: MouseEventHandler<HTMLButtonElement> = (event) => {
     onClick?.(event);
   };
 
+  const combinedClassName = classnames(styles.button, {
+    [styles.alt]: typeView === 'alt',
+  });
+
   return (
-    <ButtonView {...props} onClick={handleClick}>
+    <ButtonView {...props} className={combinedClassName} onClick={handleClick}>
       {children}
     </ButtonView>
   );
--- src/components/ui/Button/ButtonView.tsx
+++ src/components/ui/Button/ButtonView.tsx
@@ -1,7 +1,5 @@
 import { FC, ReactNode, ButtonHTMLAttributes, CSSProperties } from 'react';
 
-import styles from './styles/ButtonView.module.scss';
-
 interface IButtonStyleProps {
   width?: number | string;
   height?: number | string;
@@ -11,14 +9,14 @@ export interface IButtonViewProps extends ButtonHTMLAttributes<HTMLButtonElement
   children?: ReactNode;
 }
 
-const ButtonView: FC<IButtonViewProps> = ({ children, width, height, ...props }) => {
+const ButtonView: FC<IButtonViewProps> = ({ children, width, height, className, ...props }) => {
   const buttonStyle: CSSProperties = {
     width: width,
     height: height,
   };
 
   return (
-    <button className={styles.button} style={buttonStyle} {...props}>
+    <button className={className} style={buttonStyle} {...props}>
       {children}
     </button>
   );
