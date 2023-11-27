e05095b2a523956dc8aa68f0adfd8888e3d56c10
LexxXell <lexxxell007@gmail.com>
2023-11-27 16:27:11 +0400

    Added ui/Button

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
-import ButtonView from './ButtonView';
+import { FC, MouseEventHandler } from 'react';
+import ButtonView, { IButtonViewProps } from './ButtonView';
 
-function Button() {
-  return <ButtonView />;
-}
+interface IButtonProps extends IButtonViewProps {}
+
+const Button: FC<IButtonProps> = ({ children, onClick, ...props }) => {
+  const handleClick: MouseEventHandler<HTMLButtonElement> = (event) => {
+    onClick?.(event);
+  };
+
+  return (
+    <ButtonView {...props} onClick={handleClick}>
+      {children}
+    </ButtonView>
+  );
+};
 
 export default Button;
--- src/components/ui/Button/ButtonView.tsx
+++ src/components/ui/Button/ButtonView.tsx
@@ -1,13 +1,27 @@
-import { FC, ReactNode, ButtonHTMLAttributes } from 'react';
+import { FC, ReactNode, ButtonHTMLAttributes, CSSProperties } from 'react';
 
 import './styles/Button.scss';
 
-interface IButtonViewProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+interface IButtonStyleProps {
+  width?: number | string;
+  height?: number | string;
+}
+
+export interface IButtonViewProps extends ButtonHTMLAttributes<HTMLButtonElement>, IButtonStyleProps {
   children?: ReactNode;
 }
 
-const ButtonView: FC<IButtonViewProps> = ({ children, ...props }) => {
-  return <button {...props}>{children}</button>;
+const ButtonView: FC<IButtonViewProps> = ({ children, width, height, ...props }) => {
+  const buttonStyle: CSSProperties = {
+    width: width,
+    height: height,
+  };
+
+  return (
+    <button style={buttonStyle} {...props}>
+      {children}
+    </button>
+  );
 };
 
 export default ButtonView;
