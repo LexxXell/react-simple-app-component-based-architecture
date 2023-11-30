fad846c6cb9bbadf4fabfcfe79cda10fa5b4e7f2
LexxXell <lexxxell007@gmail.com>
2023-11-30 16:21:44 +0400

    Updated ui/Button

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,9 +1,6 @@
 import { FC, MouseEventHandler } from 'react';
 import ButtonView, { IButtonViewProps } from './ButtonView';
 
-import styles from './styles/ButtonView.module.scss';
-import classnames from 'classnames';
-
 interface IButtonProps extends IButtonViewProps {
   typeView?: 'def' | 'alt';
 }
@@ -13,12 +10,8 @@ const Button: FC<IButtonProps> = ({ children, typeView = 'def', onClick, ...prop
     onClick?.(event);
   };
 
-  const combinedClassName = classnames(styles.button, {
-    [styles.alt]: typeView === 'alt',
-  });
-
   return (
-    <ButtonView {...props} className={combinedClassName} onClick={handleClick}>
+    <ButtonView {...props} onClick={handleClick}>
       {children}
     </ButtonView>
   );
